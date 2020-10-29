b12cd442ce0c6b338a2a8e9dcf331a90f6a9a357
Matteo Carpi <matteocarpi94@gmail.com>
2020-10-29 18:33:07 +0100

    Make sure config is good

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,6 @@ module.exports = {
     author: '@matteocarpi',
   },
   plugins: [
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-transformer-remark',
@@ -35,9 +33,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
-        fonts: [
-          'Roboto',
-        ],
+        fonts: ['Josefin Sans:200,400,700', 'Josefin Slab:400,600,700'],
         display: 'swap',
       },
     },
@@ -53,17 +49,17 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'content',
+        name: 'uploads',
         // eslint-disable-next-line no-undef
-        path: `${__dirname}/content`,
+        path: `${__dirname}/content/images`,
       },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'uploads',
+        name: 'content',
         // eslint-disable-next-line no-undef
-        path: `${__dirname}/content/images`,
+        path: `${__dirname}/content`,
       },
     },
     // {
@@ -76,6 +72,76 @@ module.exports = {
     // },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    // 'gatsby-plugin-sharp',
+    // 'gatsby-transformer-sharp',
+    // 'gatsby-plugin-sass',
+    // {
+    //   resolve: 'gatsby-transformer-remark',
+    //   options: {
+    //     plugins: [
+    //       'gatsby-remark-relative-images',
+    //       'gatsby-remark-normalize-paths',
+    //       {
+    //         resolve: 'gatsby-remark-images',
+    //         options: {
+    //           // It's important to specify the maxWidth (in pixels) of
+    //           // the content container as this plugin uses this as the
+    //           // base for generating different widths of each image.
+    //           maxWidth: 1024,
+    //           linkImagesToOriginal: false,
+    //           backgroundColor: 'transparent',
+    //           plugins: ['gatsby-remark-images-anywhere'],
+    //         },
+    //       },
+    //       '@forestryio/gatsby-remark-normalize-paths',
+    //       'gatsby-remark-copy-linked-files',
+    //     ],
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-google-fonts',
+    //   options: {
+    //     fonts: [
+    //       'Roboto',
+    //     ],
+    //     display: 'swap',
+    //   },
+    // },
+    // 'gatsby-plugin-react-helmet',
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'images',
+    //     // eslint-disable-next-line no-undef
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'content',
+    //     // eslint-disable-next-line no-undef
+    //     path: `${__dirname}/content`,
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: 'uploads',
+    //     // eslint-disable-next-line no-undef
+    //     path: `${__dirname}/content/images`,
+    //   },
+    // },
+    // // {
+    // //   resolve: 'gatsby-source-filesystem',
+    // //   options: {
+    // //     name: 'posts',
+    // //     // eslint-disable-next-line no-undef
+    // //     path: `${__dirname}/content/posts`,
+    // //   },
+    // // },
+    // 'gatsby-transformer-sharp',
+    // 'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
