c5609409b8fa4de9ad1b183b30c5ecf762e9fc5c
Matteo Carpi <matteocarpi94@gmail.com>
2020-11-09 21:29:22 +0100

    Gradually fade out logo

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { motion, AnimatePresence } from 'framer-motion';
-import { useScrollPosition } from '@n8tb1t/use-scroll-position';
+import { motion, useTransform, useViewportScroll } from 'framer-motion';
 
 import BackgroundImage from 'gatsby-background-image';
 import cx from 'classnames';
@@ -16,6 +15,10 @@ const Slider = ({ slides }) => {
   const pause = 10000;
 
   const slidesNumber = slides.length;
+
+  const { scrollYProgress } = useViewportScroll();
+
+  const opacity = useTransform(scrollYProgress, [0, 0.05], [1, 0]);
 
   useEffect(() => {
     let interval = null;
@@ -35,16 +38,6 @@ const Slider = ({ slides }) => {
     return () => clearInterval(interval);
   }, [index]);
 
-  const [hasScrolled, setHasScrolled] = useState(false);
-
-  useScrollPosition(({ currPos }) => {
-    if (currPos.y < -50 && !hasScrolled) {
-      setHasScrolled(!hasScrolled);
-    } else if (currPos.y >= -50 && hasScrolled) {
-      setHasScrolled(false);
-    }
-  });
-
   return (
     <div className={styles.container}>
       {slides.map((slide, i) => (
@@ -56,20 +49,16 @@ const Slider = ({ slides }) => {
             fluid={slide.foto.childImageSharp.fluid}
             className={styles.image}
           >
-            <AnimatePresence>
-              {!hasScrolled && (
-              <motion.div
-                className={styles.logo_wrapper}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                {slide.colore_del_logo === 'Nero'
-                  ? <LogoDark className={styles.logo} />
-                  : <LogoLight className={styles.logo} />}
-              </motion.div>
-              )}
-            </AnimatePresence>
+            <motion.div
+              className={styles.logo_wrapper}
+              style={{
+                opacity,
+              }}
+            >
+              {slide.colore_del_logo === 'Nero'
+                ? <LogoDark className={styles.logo} />
+                : <LogoLight className={styles.logo} />}
+            </motion.div>
           </BackgroundImage>
         </div>
       ))}
