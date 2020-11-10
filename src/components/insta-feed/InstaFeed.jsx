e5eb3fafcd1c74e2fabd17bf1894ea88d3983228
Matteo Carpi <matteocarpi94@gmail.com>
2020-11-10 18:17:38 +0100

    Save data in app state

--- src/components/insta-feed/InstaFeed.jsx
+++ src/components/insta-feed/InstaFeed.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,6 +7,7 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import styles from './InstaFeed.module.scss';
 
 const InstaFeed = () => {
+  const [instaData, setInstaData] = useState();
   const data = useStaticQuery(graphql`
     query {
       markdownRemark(frontmatter: {title: {eq: "Impostazioni"}}) {
@@ -20,14 +21,13 @@ const InstaFeed = () => {
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
-      redirect: 'follow',
     };
 
     fetch('https://graph.instagram.com/me/media?fields=id,media_url,media_type,caption&access_token=IGQVJVVkM2YU5ESGRZASktHTHRZAcGgyeWN4UlhlaEM5SG1UQ2xnVHdLbVZAxNFdUVEhMNFI3a1VDd1N2Mm1sb3NoQU5rOWVpTzNnWWRBVmsyRlFxSlgyS3Vqa0F0SUpTNGdCeHNNZADFVYTBEVlFlemQySQZDZD', requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((response) => response.json())
+      .then((result) => setInstaData(result))
       .catch((error) => console.log('error', error));
-  });
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -36,6 +36,10 @@ const InstaFeed = () => {
         <h1>@ledue_handbags</h1>
       </a>
 
+      {instaData?.data?.map((picture) => {
+        console.log(picture.url);
+        return (<img alt={picture.caption} key={picture.id} src={picture.url} />);
+      })}
     </div>
   );
 };
