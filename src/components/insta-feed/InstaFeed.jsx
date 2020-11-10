df1e3d24fc2bdb3f68ab9eec9c75908e74e57e8e
Matteo Carpi <matteocarpi94@gmail.com>
2020-11-10 17:36:10 +0100

    Try new query

--- src/components/insta-feed/InstaFeed.jsx
+++ src/components/insta-feed/InstaFeed.jsx
@@ -23,25 +23,12 @@ const InstaFeed = () => {
       redirect: 'follow',
     };
 
-    fetch('https://graph.instagram.com/786315888592536/media?fields=id,caption,media_url&access_token=IGQVJVVkM2YU5ESGRZASktHTHRZAcGgyeWN4UlhlaEM5SG1UQ2xnVHdLbVZAxNFdUVEhMNFI3a1VDd1N2Mm1sb3NoQU5rOWVpTzNnWWRBVmsyRlFxSlgyS3Vqa0F0SUpTNGdCeHNNZADFVYTBEVlFlemQySQZDZD', requestOptions)
+    fetch('https://graph.instagram.com/me/media?fields=id,media_url,media_type,caption&access_token=IGQVJVVkM2YU5ESGRZASktHTHRZAcGgyeWN4UlhlaEM5SG1UQ2xnVHdLbVZAxNFdUVEhMNFI3a1VDd1N2Mm1sb3NoQU5rOWVpTzNnWWRBVmsyRlFxSlgyS3Vqa0F0SUpTNGdCeHNNZADFVYTBEVlFlemQySQZDZD', requestOptions)
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.log('error', error));
   });
 
-  // const [data, setData] = useState();
-  // // eslint-disable-next-line no-new
-  // useEffect(() => {
-  //   const requestOptions = {
-  //     method: 'GET',
-  //     redirect: 'follow',
-  //   };
-
-  //   fetch('https://www.instagram.com/graphql/query/?query_hash=56a7068fea504063273cc2120ffd54f3&variables=%7B%22id%22%3A%2231044158025%22%2C%22first%22%3A12%7D', requestOptions)
-  //     .then((response) => response.json())
-  //     .then((result) => console.log(result))
-  //     .catch((error) => console.log('error', error));
-  // });
   return (
     <div className={styles.container}>
       <a className={styles.container} target="_blank" rel="noreferrer" href={data.markdownRemark.frontmatter.instagram}>
